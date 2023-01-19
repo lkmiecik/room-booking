ab483a63906c38574a410e7e957a072ff0e4a4d2
lkmiecik <kmiecik.lukas@gmail.com>
2023-01-19 15:03:53 +0100

    room and building model

--- RoomBooking/rooms/models.py
+++ RoomBooking/rooms/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Building(models.Model):
+    number = models.CharField(max_length=32)
+
+
+class Room(models.Model):
+    number = models.CharField(max_length=32)
+    area = models.DecimalField(null=True, decimal_places=2, max_digits=10)
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+
