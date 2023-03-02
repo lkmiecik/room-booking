a84861e8eb72405b412ccffe52a66b4ceea05e95
lkmiecik <kmiecik.lukas@gmail.com>
2023-03-02 14:20:02 +0100

    admin account

--- RoomBooking/rooms/models.py
+++ RoomBooking/rooms/models.py
@@ -7,5 +7,5 @@ class Building(models.Model):
 
 class Room(models.Model):
     number = models.CharField(max_length=32)
-    area = models.DecimalField(null=True, decimal_places=2, max_digits=10)
+    area = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=10)
     building = models.ForeignKey(Building, on_delete=models.CASCADE)
